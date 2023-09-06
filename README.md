ec561fdac656e37271b295d979a8c85f67de8239
code-hitarth <hitarth.k@inheritx.com>
2023-09-06 20:37:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sync Time
+
+I have created three apis:
+
+1) For setting users clock time /set-time
+2) For viewing current clock time /get-time
+3) For sync time of two users /sync-time
+
+Please note that, 
+# First set-time api must be used in order to get user details 
+# I have used caching in nodejs, so every time any changes will be done in code, all data will be deleted, then one will have to add new data again
+
+
+#  Run npm run start to start node server
+
+If any queries, pls reach out to me on : hitarthkansara27@gmail.com
+
+# Thank you
---
+++ index.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const moment = require('moment');
+let nodeCache = require('node-cache');
+
+const app = express();
+
+let cache = new nodeCache();
+
+app.use(express.json());
+
+
+//user_id, clock_time, utc, increment
+
+function setClockIncrement(user_id, clock_time, utc, increment) {
+    try {
+
+        let data = {
+            clock_time: moment(clock_time, 'HH:mm:ss').format('x'),
+            utc: moment.utc().format('x'),
+            increment
+        };
+        console.log('data--->', data);
+        cache.set(user_id, JSON.stringify(data));
+        return clock_time;
+
+    } catch (err) {
+        console.log('Error(setClockIncrement): ', err);
+    }
+}
+
+
+function getUserDetails(user_id) {
+    try {
+
+        let user = cache.get(user_id);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        user = JSON.parse(user);
+
+        user.clock_time = moment(user.clock_time, 'x').format('HH:mm:ss');
+
+        return user;
+
+    } catch (err) {
+        console.log('Error(getUserDetails): ', err);
+    }
+}
+
+
+
+function getUserClockTime(userId) {
+
+    try {
+
+        let user = cache.get(userId);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        let { utc, increment, clock_time } = JSON.parse(user);
+
+        let utcTimeNow = utc;
+
+        let startTime = moment(clock_time, 'x');
+        let endTime = moment(utcTimeNow, 'x');
+
+        let currentTime = moment(moment().format('x'), 'x');
+
+        let timeDuration = moment.duration(currentTime.diff(endTime)).as('milliseconds');
+        console.log('timeDuration--->', timeDuration);
+
+        let timeToAdd = timeDuration / increment;
+
+        return moment(startTime, 'x').add(timeToAdd, 'milliseconds');
+    } catch (err) {
+        console.log('Error(getUserClockTime): ', err);
+    }
+}
+
+
+//Please use this api first to set time and increment of user
+// Req Body -> {
+//     "user_id": "1",
+//     "current_time": "18:00:00",
+//     "increment": 5
+// }
+
+app.post('/set-time', (req, res) => {
+    try {
+
+        let { user_id, increment, current_time } = req.body;
+
+        setClockIncrement(user_id, current_time, moment().utc(), increment);
+
+        return res.status(200).send({ message: 'Clock time set successfully', data: current_time });
+
+    } catch (err) {
+        console.log('Error(set-time): ', err);
+        return res.status(500).send({ message: 'Server is not responding', error: err });
+    }
+})
+
+
+// Use this api to view clock time of user
+app.get('/get-time', (req, res) => {
+    try {
+
+        let { user_id } = req.query;
+
+        let userClockTime = getUserClockTime(user_id);
+
+        if (!userClockTime) return res.status(400).send({ message: 'User data not found' })
+
+        return res.status(200).send({ message: 'Clock time get successfully', data: moment(userClockTime).format('HH:mm:ss') });
+
+    } catch (err) {
+        console.log('Error(get-time): ', err);
+        return res.status(500).send({ message: 'Server is not responding', error: err });
+    }
+})
+
+
+
+//Req Body -> {
+//     "current_user_id": "2",
+//     "user_id": "1"
+// }
+
+// Use this api to sync time of two users
+app.post('/sync-time', (req, res) => {
+    try {
+
+        let { current_user_id, user_id } = req.body;
+
+        let anotherUserClockTime = getUserClockTime(user_id);
+
+        let currentUserDetails = getUserDetails(current_user_id);
+        let anotherUserDetails = getUserDetails(user_id);
+
+        let currentUtcTime = moment.utc().format('x');
+
+        let currentUserData = {
+            clock_time: moment(anotherUserClockTime).format('x'),
+            utc: currentUtcTime,
+            increment: currentUserDetails.increment
+        };
+
+        let anotherUserData = {
+            clock_time: moment(anotherUserClockTime).format('x'),
+            utc: currentUtcTime,
+            increment: anotherUserDetails.increment
+        };
+
+        setClockIncrement(current_user_id, moment(currentUserData.clock_time, 'x').format('HH:mm:ss'), currentUserData.utc, currentUserData.increment);
+        setClockIncrement(current_user_id == '1' ? '2' : '1', moment(currentUserData.clock_time, 'x').format('HH:mm:ss'), anotherUserData.utc, anotherUserData.increment);
+
+        return res.status(200).send({ message: 'Clock time sync successfully', data: moment(anotherUserClockTime).format('HH:mm:ss') });
+
+    } catch (err) {
+        console.log('Error(sync-time): ', err);
+        return res.status(500).send({ message: 'Server is not responding', error: err });
+    }
+});
+
+
+
+app.listen(4000, () => console.log('Server running on port: ', 4000));
